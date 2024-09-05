b81a6882667a5a2d1bd72a5f6048f0046113e9b8
Santiago <sammysan007@gmail.com>
2024-09-04 21:37:52 -0300

    feature(UserDecorator): added return value as UserSessionModel

--- src/infra/core/authorization/decorators/user/user.decorator.ts
+++ src/infra/core/authorization/decorators/user/user.decorator.ts
@@ -1,10 +1,14 @@
 import { createParamDecorator, ExecutionContext } from '@nestjs/common';
-import { UserEntity } from '../../../../../modules/user/entities/user.entity';
+import { UserSessionModel } from '../../../../../modules/user/models/user-session.model';
 
 export const User = createParamDecorator(
-  (data: unknown, context: ExecutionContext): UserEntity => {
+  (data: unknown, context: ExecutionContext): UserSessionModel => {
     const request = context.switchToHttp().getRequest();
 
-    return request.user;
+    return {
+      id: request.user.sub,
+      email: request.user.username,
+      role: request.user.role,
+    } as UserSessionModel;
   },
 );
